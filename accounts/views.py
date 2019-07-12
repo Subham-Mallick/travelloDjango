ae0a972578cc0de6cc00bfb7148c0361a7d7eb3e
Subham Mallick <subham2mallick@gmail.com>
2019-07-13 01:15:35 +0530

    fixed bug to show message to check if username/email already exists along with check of password with confirm password

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
@@ -14,15 +15,18 @@ def register(request):
 
         if password1 == password2:
             if User.objects.filter(username=username).exists():
-                print("Username Taken")
+                messages.info(request,"Username Taken")
+                return redirect('register')
             elif User.objects.filter(email=email).exists():
-                print("Email Taken")
+                messages.info(request,"Email Taken")
+                return redirect('register')
             else:
                 user = User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
                 user.save()
-                print("User Created")
+                messages.info(request,"User Created")
         else:
-            print("Password not matching")
+            messages.info(request,"Password not matching")
+            return redirect('register')
         return redirect('/')
     else:
         return render(request,'register.html')
